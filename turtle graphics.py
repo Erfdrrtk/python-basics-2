a242dc56f187097ec6261de1244405019527a6ab
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-03 14:20:58 -0600

    I made a box :D

---
+++ turtle graphics.py
@@ -0,0 +1,22 @@
+# whenever you make a turtle, do import turtle
+# simple commmands:
+# bob.forward(100) # this makes the cursor go forward by 100 pixels
+# bob.left(45) # this makes bob face to the left by 45 degrees
+# bob.right(90) # this makes bob face to the right by 90 degrees
+import turtle
+
+bob = turtle.Turtle()
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+
+print('this code is running. close the turtle graphics app to end it')
+
+
+
+
+turtle.done() # this finishes the code
