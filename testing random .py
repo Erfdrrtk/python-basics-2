84d5b3c34a9cd40b037f13bb13ee5b87ff4d5434
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-08 19:28:28 -0600

    stuff

--- testing random .py
+++ testing random .py
@@ -1,2 +1,6 @@
-age = 123
-name = 'erfdrrtk'
+print("--- welcome to simple calculator ---")
+first = float(input('enter the first number: '))
+second = float(input('enter the second number: '))
+sum = first + second
+
+print(f"sum: {sum}")
